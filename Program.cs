d9f35c6885dffdbe3aa99498b890eeb048f9ac1c
GuilhermeMassashiYano <gui.massashi.yano@gmail.com>
2026-08-19 22:05:37 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿// ALT, SHIFT + F --> Identação
-// Comentário teste commit Aula 03
-
-using System;
+﻿using System;
+using Microsoft.VisualBasic;
 
 namespace ATV1
 {
@@ -9,40 +7,47 @@ namespace ATV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Observe o meno abaixo e digite o número referente a opção desejada: ");
+            Console.WriteLine("Observe o menu abaixo e digite o número referente a opção desejada: ");
             Console.WriteLine("1- Concatenar Palavras ");
             Console.WriteLine("2- Verificar Dia Da Semana");
             Console.WriteLine("3- Calcular Média");
             Console.WriteLine("4- Calcular Tabuada");
+            Console.WriteLine("5- Detalhar Data");
+            Console.WriteLine("6- Calcular Desconto INSS");
 
             int opcaoEscolhida = int.Parse(Console.ReadLine());
 
             switch (opcaoEscolhida)
             {
                 case 1:
-                ConcatenarPalavras();
-                break;
+                    ConcatenarPalavras();
+                    break;
 
                 case 2:
-                VerificarAulaEtec();
-                break;
+                    VerificarAulaEtec();
+                    break;
 
-                case 3 :
-                CalcularMedia();
-                break;
+                case 3:
+                    CalcularMedia();
+                    break;
 
                 case 4:
-                CalcularTabuada();
-                break;
+                    CalcularTabuada();
+                    break;
+
+                case 5:
+                    DetalharData();
+                    break;
+
+                case 6:
+                    CalcularDescontoINSS();
+                    break;
                 
                 default:
-                Console.WriteLine("Opção Inválida ");
-                break;
+                    Console.WriteLine("Opção Inválida ");
+                    break;
             }
-         
         }
-
-
 
         public static void VerificarAulaEtec()
         {
@@ -51,13 +56,14 @@ namespace ATV1
 
             if (data.DayOfWeek == DayOfWeek.Saturday || data.DayOfWeek == DayOfWeek.Sunday)
             {
-                Console.WriteLine("final de semana! Hoje nao tem aula! Revisarei exercicios.");
+                Console.WriteLine("Final de semana! Hoje nao tem aula! Revisarei exercicios.");
             }
             else
             {
                 Console.WriteLine("Dia da semana! Bora pra etec!");
             }
         }
+
         public static void CalcularTabuada()
         {
             Console.WriteLine("Digite a tabuada de sua preferencia para efetivar os calculos:");
@@ -67,12 +73,10 @@ namespace ATV1
             while (contador <= 10)
             {
                 string mensagem = string.Format("{0} x {1} = {2}", tabuada, contador, tabuada * contador);
-
                 Console.WriteLine(mensagem);
                 contador++;
             }
         }
-
 
         public static void ConcatenarPalavras()
         {
@@ -83,13 +87,13 @@ namespace ATV1
             Console.WriteLine(frase1);
             Console.WriteLine("=====================================================");
 
-            Console.WriteLine("quanto custa um dolár em reais?");
+            Console.WriteLine("Quanto custa um dólar em reais?");
             decimal valorDolarReais = decimal.Parse(Console.ReadLine());
-            string frase2 = string.Format("Hoje é {0: dd/MM/yyyy},o dolár está custando {1:c2}", DateTime.Now, valorDolarReais);
+            string frase2 = string.Format("Hoje é {0:dd/MM/yyyy}, o dólar está custando {1:c2}", DateTime.Now, valorDolarReais);
             Console.WriteLine(frase2);
             Console.WriteLine("==========================================");
 
-            string cabecalho = string.Format("{0:dddd}, {0:dd} de {0:MMMM} de {0:yy} - {0:HH:mm;ss}", DateTime.Now);
+            string cabecalho = string.Format("{0:dddd}, {0:dd} de {0:MMMM} de {0:yy} - {0:HH:mm:ss}", DateTime.Now);
             Console.WriteLine(cabecalho);
         }
 
@@ -98,7 +102,7 @@ namespace ATV1
             Console.WriteLine("Digite sua primeira nota:");
             decimal nota1 = decimal.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite sua 2 nota:");
+            Console.WriteLine("Digite sua 2ª nota:");
             decimal nota2 = decimal.Parse(Console.ReadLine());
 
             decimal media = (nota1 + nota2) / 2;
@@ -111,7 +115,49 @@ namespace ATV1
                 Console.WriteLine("Reprovado");
         }
 
+        public static void DetalharData()
+        {
+            Console.WriteLine("Digite uma data (ex: DD/MM/AAAA):");
+            DateTime dataInformada = DateTime.Parse(Console.ReadLine());
 
+            string resultadoData = string.Format("Dia da semana: {0:dddd} | Mês: {0:MMMM}", dataInformada);
+            Console.WriteLine(resultadoData);
+
+            if (dataInformada.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine($"É domingo! Hora atual: {DateTime.Now:HH:mm}");
+            }
+        }
+
+        public static void CalcularDescontoINSS()
+        {
+            Console.WriteLine("Digite o valor do salário:");
+            decimal salario = decimal.Parse(Console.ReadLine());
+            decimal aliquota = 0;
+            decimal valorInss = 0;
+
+            if (salario <= 1412.00m)
+            {
+                aliquota = 0.075m; // 7.5%
+            }
+            else if (salario <= 2666.68m)
+            {
+                aliquota = 0.09m; // 9%
+            }
+            else if (salario <= 4000.03m)
+            {
+                aliquota = 0.12m; // 12%
+            }
+            else
+            {
+                aliquota = 0.14m; // 14%
+            }
+
+            valorInss = salario * aliquota;
+            decimal salarioLiquido = salario - valorInss;
+
+            Console.WriteLine($"Valor do INSS a pagar: {valorInss:c2}");
+            Console.WriteLine($"Salário com o desconto do INSS: {salarioLiquido:c2}");
+        }
     }
 }
-
